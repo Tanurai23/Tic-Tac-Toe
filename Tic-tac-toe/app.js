5eb4dfd6deac01cde44bd81fd0a7d897fbca5a94
Tanu Rai <tanurai9857@gmail.com>
2025-12-05 14:06:19 +0530

    Enhance game with sound effects and visual feedback
    Added sound effects for clicks and winning event, and improved visual feedback for player turns.

--- Tic-tac-toe/app.js
+++ Tic-tac-toe/app.js
@@ -1,79 +1,80 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn= document.querySelector("#reset-Btn");
-let newGameBtn = document.querySelector("#new-btn");
+let resetBtn = document.querySelector("#reset-Btn");
+let newGameBtn = document.querySelector("#new-Btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; //playerX, playerO
+let clickSound = document.querySelector("#click-sound");
+let winSound = document.querySelector("#win-sound");
 
-const winPatterns=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8], 
+let turnO = true;
+
+const winPatterns = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
 ];
 
-const resetGame= () =>{
-    turnO = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
 };
 
 boxes.forEach((box) => {
-    box.addEventListener("click",() => {
-        console.log("Box was clicked");
-       if(turnO){
-        box.innerText= "O";
-        turnO = false;
-       }else{
-        box.innerText ="X";
-        turnO = true;
-       }
-       box.disabled = true;
+  box.addEventListener("click", () => {
+    clickSound.play();
 
+    if (turnO) {
+      box.innerText = "O";
+      box.style.color = "#00eaff";
+    } else {
+      box.innerText = "X";
+      box.style.color = "#ff00ff";
+    }
 
-       checkWinner();
-    });
+    box.disabled = true;
+    checkWinner();
+    turnO = !turnO;
+  });
 });
 
-const disableBoxes = () =>{
-    for(let box of boxes){
-        box.disabled = true;
-    }
+const disableBoxes = () => {
+  boxes.forEach((box) => box.disabled = true);
 };
-    
-const enableBoxes = () =>{
-    for(let box of boxes){
-        box.disabled = false;
-        box.innerText= "";
-    }
-};
-    
-const showWinner = (winner) =>{
-    msg.innerText =`Congratulations, Winner is ${winner}`;
-    msgContainer.classList.remove("hide"); 
-};
-    
 
-const checkWinner = () =>{
-    for (let pattern of winPatterns) {
-        let pos1Val= boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val= boxes[pattern[2]].innerText;
-        
-        if(pos1Val !="" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                console.log("Winner");
-                showWinner(pos1Val);
-                 disableBoxes();
-            }
-        }
-    }
+const enableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+    box.classList.remove("win-glow");
+  });
+};
 
+const showWinner = (winner, pattern) => {
+  msg.innerText = `🎉 Winner: ${winner}`;
+  msgContainer.classList.remove("hide");
+
+  winSound.play();
+
+  // Apply glowing effect only to winning boxes
+  pattern.forEach(i => boxes[i].classList.add("win-glow"));
+
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let [a, b, c] = pattern;
+    let val1 = boxes[a].innerText;
+    let val2 = boxes[b].innerText;
+    let val3 = boxes[c].innerText;
+
+    if (val1 && val1 === val2 && val2 === val3) {
+      showWinner(val1, pattern);
+      return;
+    }
+  }
 };
 
 newGameBtn.addEventListener("click", resetGame);
